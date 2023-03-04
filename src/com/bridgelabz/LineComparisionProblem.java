71142200863ee1ac30fb2a47b87ef1534e99eb75
prathameshadam10 <prathameshadam10@gmail.com>
2023-03-04 18:58:16 +0530

    Welcome To Line Comparision Program

---
+++ src/com/bridgelabz/LineComparisionProblem.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class LineComparisionProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line comparision Computation Program");
+    }
+}
