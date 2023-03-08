3de065c2f32a216110ef2967fbc76423e05cf15c
prathameshadam10 <121919043+prathameshadam10@users.noreply.github.com>
2023-03-08 20:36:56 +0530

    Update LineComparisionProblem.java

--- src/com/bridgelabz/LineComparisionProblem.java
+++ src/com/bridgelabz/LineComparisionProblem.java
@@ -3,42 +3,87 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class LineComparisionProblem {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Line comparision Computation Program");
+    static double x1, x2, y1, y2;
+    static double length1;
+    public static double Lengthofline1(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter value of x1 :");
-        int x1 = sc.nextInt();
-        System.out.println("Enter value of y1 :");
-        int y1 = sc.nextInt();
-        System.out.println("Enter value of x2 :");
-        int x2 = sc.nextInt();
-        System.out.println("Enter value of y2 :");
-        int y2 = sc.nextInt();
-        System.out.println("Enter value of x3 :");
-        int x3 = sc.nextInt();
-        System.out.println("Enter value of y3 :");
-        int y3 = sc.nextInt();
-        System.out.println("Enter value of x4 :");
-        int x4 = sc.nextInt();
-        System.out.println("Enter value of y4 :");
-        int y4 = sc.nextInt();
+        System.out.println("Enter the value of x1 :");
+        x1 = sc.nextInt();
+        System.out.println("Enter the value of y1 :");
+        y1 = sc.nextInt();
+        System.out.println("Enter the value of x2 :");
+        x2 = sc.nextInt();
+        System.out.println("Enter the value of y2 :");
+        y2 = sc.nextInt();
+        length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println("The length of a line using cartesian system = " + length1);
+        return 0;
+    }
 
-        Double length1 = Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
-        System.out.println("Length of first line: " + length1);
 
-        Double length2 = Math.sqrt((Math.pow((x4 - x3), 2)) + (Math.pow((y4 - y3), 2)));
-        System.out.println("Length of second line is: " + length2);
 
-        int result = length2.compareTo(length1);
+    static double x3, x4, y3, y4;
+    static double length2;
+    public static void Lengthofline2(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of x3 :");
+        x3 = sc.nextInt();
+        System.out.println("Enter the value of y3 :");
+        y3 = sc.nextInt();
+        System.out.println("Enter the value of x4 :");
+        x4 = sc.nextInt();
+        System.out.println("Enter the value of y4 :");
+        y4 = sc.nextInt();
+        length2 = Math.sqrt(Math.pow(x4-x3,2)+Math.pow(y4-y3,2));
+        System.out.println("The length of a line ="+length2);
+    }
 
-        if (result == 0) {
-            System.out.println("\nLines are equal");
-        } else if (result > 0) {
-            System.out.println("\nLine 2 is greater than line 1");
-        } else {
-            System.out.println("\nline 1 is greater than line 2");
 
+
+
+    public void equalityCheckofLines(){
+
+        double A = LineComparisionProblem.length1;
+        double B = LineComparisionProblem.length2;
+
+        String LineA = String.valueOf(A);
+        String LineB = String.valueOf(B);
+
+        if(LineA.equals(LineB)){
+            System.out.println("Length of both lines are equal");
+        }else{
+            System.out.println("lengths are not equal");
         }
-        
+    }
+
+
+
+    public void compareofLines(){
+        String LineA=String.valueOf(length1);
+        String LineB=String.valueOf(length2);
+//        double A = LineComparisonDAY8.lengthofLine1;
+//        double B = LineComparisonDAY8.LengthofLine2;
+        int compare=LineA.compareTo(LineB);
+        System.out.println(compare);
+        if(compare==0) {
+            System.out.println("lines are equal");
+        }else if(compare>0) {
+            System.out.println("lineA is greater then lineB");
+        }else {
+            System.out.println("lineA is less than lineB");
+        }
+    }
+
+
+
+    public static void main(String[] args){
+        /*Start with Displaying Welcome to Line Comparison Computation Program on main  Branch */
+        System.out.println("Welcome to line computation problem");
+
+        Lengthofline1();
+        Lengthofline2();
+        LineComparisionProblem obj = new LineComparisionProblem();
+        obj.equalityCheckofLines();
+        obj.compareofLines();
     }
 }
